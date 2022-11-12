90ac5b43c56347a6fec4fb0af827edf6cda7473b
Jorge <46224205+jidpdev@users.noreply.github.com>
2022-11-12 13:16:01 -0300

    Update xcblottery.sol

--- xcblottery.sol
+++ xcblottery.sol
@@ -12,91 +12,91 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Lottery is ReentrancyGuard {
 
-    address public owner;
-    IERC20 public tokenContract;
-	
+	address public owner;
+	IERC20 public tokenContract;
+
 	uint256 public playersTotal;
 	uint256 public winnersTotal;
 	uint256 public ticketPrice;
-	
+
 	address[] private players;
 	address[] private winners;
 	uint256[] private prizesPercentiles;
 	uint256 public lotteryID;
-	
+
 	bool public isOpen;
 	bool public isCompleted;
-	
+
 	event Sent(address _from, address _to, uint256 _amount);
 	event NewPlayer(address _player, uint256 _lotteryID);
 	event NewWinner(address _winner, uint256 _lotteryID);
 
 	mapping(address => bool) private blackList;
 
-    constructor ( IERC20 _tokenContract ) {
-	    owner = msg.sender;
-        tokenContract = _tokenContract;
-    }
+	constructor ( IERC20 _tokenContract ) {
+		owner = msg.sender;
+		tokenContract = _tokenContract;
+	}
 	
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Error. Caller is not the owner.");
-        _;
-    }	
+	modifier onlyOwner() {
+		require(msg.sender == owner, "Error. Caller is not the owner.");
+		_;
+	}	
 
 	function setNewLottery ( uint256 _playersTotal,	uint256 _winnersTotal, uint256 _ticketPrice, uint256[] memory _prizesPercentiles ) external onlyOwner {
 		require(_winnersTotal < _playersTotal, "Error. More winners than players.");
 		require(_winnersTotal == _prizesPercentiles.length, "Error. Inconsistency in the prize list.");
 		require(_ticketPrice > 0, "Error. Ticket price is zero.");
-		
+
 		uint256 _totalPrizes = 0;
-        for (uint256 _i = 0; _i < _prizesPercentiles.length; _i++) {
+		for (uint256 _i = 0; _i < _prizesPercentiles.length; _i++) {
 			_totalPrizes += _prizesPercentiles[_i];
 		}
 		require(_totalPrizes == 100, "Error. Prizes distribution isn't 100%.");
-		
+
 		playersTotal = _playersTotal;
 		winnersTotal = _winnersTotal;
 		ticketPrice = _ticketPrice;
 		prizesPercentiles = _prizesPercentiles;
 		lotteryID = block.timestamp;
-		
+
 		delete players;
-		
+
 		isOpen = true;
 		isCompleted = false;
-	
+
 	}
 
-    function joinGame() external nonReentrant {
+	function joinGame() external nonReentrant {
 		require(isOpen, "Error. Lottery is closed.");
-        require(players.length < playersTotal, "Error. Exceed total players.");
+		require(players.length < playersTotal, "Error. Exceed total players.");
 		require(!findWallet(msg.sender, players), "Error. You have already participated.");
 		require(!blackList[msg.sender], "Error. Address blacklisted.");
-        tokenContract.transferFrom(msg.sender, address(this), ticketPrice);
-        players.push(msg.sender);
+		tokenContract.transferFrom(msg.sender, address(this), ticketPrice);
+		players.push(msg.sender);
 		emit NewPlayer(msg.sender, lotteryID);
-		
-        if(players.length == playersTotal){
+
+		if(players.length == playersTotal){
 			isOpen = false;
 			isCompleted = true;
 		}
-    }
+	}
 
 	function drawWinners() external onlyOwner {
 		require(isCompleted, "Error. Number of player not is completed.");
 		uint256 _winnerIndex;
-        uint256 _count = 0;
+		uint256 _count = 0;
 		uint256 _nounce = 0;
-		
+
 		delete winners;
-	   
-        while(_count < winnersTotal){
+
+		while(_count < winnersTotal){
 			_winnerIndex = generateRandom(_count, _nounce);
-            if (_count == 0) {
+	   		 if (_count == 0) {
 				winners.push( players[_winnerIndex] );
-                _count++;
+				_count++;
 				emit NewWinner(players[_winnerIndex], lotteryID);
-            } else {
+	    		} else {
 				if (!findWallet(players[_winnerIndex], winners)) {
 					winners.push( players[_winnerIndex] );
 					_count++;
@@ -107,7 +107,7 @@ contract Lottery is ReentrancyGuard {
 			}
 		}
 
-    }
+	}
 
 	function generateRandom(uint256 _index, uint256 _nounce) private view returns(uint256){
 		bytes32 _random = keccak256(abi.encodePacked(_index, _nounce, block.number, block.difficulty, block.timestamp));
@@ -125,44 +125,44 @@ contract Lottery is ReentrancyGuard {
 		return _finded;
 	}
 
-    function payPrizes() external onlyOwner {
+	function payPrizes() external onlyOwner {
 		uint256 _pool = tokenContract.balanceOf(address(this));
 		uint256 _prizeAmount;
 		require(isCompleted, "Error. Number of player not is completed.");
 		require(winners.length == prizesPercentiles.length, "Error. The winners have not been drawn.");
 		require(_pool > 0, "Error. Insufficient balance.");
-        for (uint256 _i = 0; _i < winners.length; _i++) {
+		for (uint256 _i = 0; _i < winners.length; _i++) {
 			_prizeAmount = prizesPercentiles[_i] * _pool / 100;
-            tokenContract.transfer(winners[_i], _prizeAmount);
+	    		tokenContract.transfer(winners[_i], _prizeAmount);
 			emit Sent(address(this), winners[_i], _prizeAmount);
 		}
 		isCompleted = false;
-    }
+	}
 
-    function withdrawContract(uint256 _tokenAmount) external onlyOwner {
-        tokenContract.transfer(address(msg.sender), _tokenAmount);
-    }
-	
-    function getTokenBalance() external view returns (uint256) {
-        return tokenContract.balanceOf(address(this)); 
-    }	
+	function withdrawContract(uint256 _tokenAmount) external onlyOwner {
+		tokenContract.transfer(address(msg.sender), _tokenAmount);
+	}
 
-    function getNumberOfPlayers() external view returns (uint256) {
-        return players.length; 
-    }	
+	function getTokenBalance() external view returns (uint256) {
+		return tokenContract.balanceOf(address(this)); 
+	}	
+
+	function getNumberOfPlayers() external view returns (uint256) {
+		return players.length; 
+	}	
 
 	function getLastWinners() external view returns (address[] memory) {
 		return winners;
 	}
 
-    function isPlayer(address _wallet) external view returns (bool) {
-        return findWallet(_wallet, players ); 
-    }
+	function isPlayer(address _wallet) external view returns (bool) {
+		return findWallet(_wallet, players ); 
+	}
 
-    function isWinner(address _wallet) external view returns (bool) {
-        return findWallet(_wallet, winners ); 
-    }
-	
+	function isWinner(address _wallet) external view returns (bool) {
+		return findWallet(_wallet, winners ); 
+	}
+
 	function removeBlackList(address _account) external onlyOwner {
 		blackList[_account] = false;
 	}
